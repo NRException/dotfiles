2413328df4b4424ba9414bd76a7d81e3d0bd826b
Cameron Huggett <cameron.huggett@zonal.co.uk>
2024-04-23 09:40:22 +0100

    add rosepine transparency (properly this time)

--- .config/nvim/lua/nrexception/init.lua
+++ .config/nvim/lua/nrexception/init.lua
@@ -2,6 +2,8 @@ print("Loading NRException profile...")
 require("nrexception.remap")
 require("nrexception.packer")
 
+vim.cmd('colorscheme rose-pine')
+
 -- Add relative line numbers and remove ugly vim fill chars.
 vim.wo.relativenumber = true
 vim.opt.fillchars = {eob = " "}
