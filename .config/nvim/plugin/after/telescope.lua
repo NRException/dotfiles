6d6deb9ed71056ad2c4d4bf55c295af6cb8dd3cc
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 <cameron.huggett@zonal.co.uk>
2025-02-07 12:43:15 +0000

    add git status keybind

--- .config/nvim/plugin/after/telescope.lua
+++ .config/nvim/plugin/after/telescope.lua
@@ -3,3 +3,4 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Telescope [F]ind
 vim.keymap.set('n', '<leader>fg', ":Telescope live_grep_args<CR>", { desc = "Telescope [F]ind [G]rep" })
 vim.keymap.set('n', '<leader>gb', ":Telescope git_branches<CR>", { desc = "Telescope [G]ind [B]ranches" })
 vim.keymap.set('n', '<leader>gc', ":Telescope git_commits<CR>", { desc = "Telescope [G]it [C]ommits" })
+vim.keymap.set('n', '<leader>gs', ":Telescope git_status<CR>", { desc = "Telescope [G]it [S]status" })
